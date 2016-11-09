2c51552ab8c818ddfe297fc96439cd95b4634a73
Marius <marius@192.168.2.26>
2016-11-09 22:38:27 +0100

    added malc0de

--- getfeeds.py
+++ getfeeds.py
@@ -1,7 +1,9 @@
-from OTXv2 import OTXv2, IndicatorTypes
-from collections import defaultdict
 import pprint
+import requests
+from OTXv2 import OTXv2
+from collections import defaultdict
 from datetime import datetime, timedelta
+
 """
     File name: getfeeds.py
     Author: Marius Iversen
@@ -10,27 +12,56 @@ from datetime import datetime, timedelta
     Python Version: 2.7.12
 """
 
-# Define API Key for AlienVault OTX
-otx = OTXv2("aec68b3658767656b3d93aced4c8f2ea11232c1e2e46acb078692a30a028a174")
-
-mtime = (datetime.now() - timedelta(days=62)).isoformat()
-# Get the different feeds and store them in memory
-
-pulses = otx.getsince(mtime)
-
+# Initate the list that will store all the information to be printed out later
 d = defaultdict(list)
 
-for index, pulseshit in enumerate(pulses):
-    for pulse in pulses[index]['indicators']:
-        if pulse['type'] == 'IPv4':
-            d['IPv4'].append(pulse['indicator'])
-        elif pulse['type'] == 'URL':
-            d['URL'].append(pulse['indicator'])
-        elif pulse['type'] == 'domain':
-            d['domain'].append(pulse['indicator'])
 
-pp = pprint.PrettyPrinter(indent=4)
+# Function to grab files from the internet
+def download_file(url):
+    r = requests.get(url)
+    return r.content
 
-pp.pprint (['IPv4'])
-pp.pprint (d['URL'])
-pp.pprint (d['domain'])
+
+# AlienVault Feed
+def get_alienvault():
+    # Define API Key for AlienVault OTX
+    otx = OTXv2("aec68b3658767656b3d93aced4c8f2ea11232c1e2e46acb078692a30a028a174")
+
+    # get current time, - the amount of days you want to look back
+    mtime = (datetime.now() - timedelta(days=62)).isoformat()
+
+    # Get the different feeds and store them in memory
+    pulses = otx.getsince(mtime)
+
+    # Populate the list with the returned results from OTX
+    for index, pulseshit in enumerate(pulses):
+        for pulse in pulses[index]['indicators']:
+            if pulse['type'] == 'IPv4':
+                d['alienvaultip'].append(pulse['indicator'])
+            elif pulse['type'] == 'URL':
+                d['alienvaulturl'].append(pulse['indicator'])
+            elif pulse['type'] == 'domain':
+                d['alienvaultdomain'].append(pulse['indicator'])
+    return d
+
+
+# Get Malc0de feed IP addresses
+def get_malc0de():
+    url = 'http://malc0de.com/bl/IP_Blacklist.txt'
+    results = download_file(url).splitlines()
+    for result in results:
+        result = result.lstrip().rstrip()
+        if not result.startswith('//') and len(result) > 0:
+            d['malc0deips'].append(result)
+    return d
+
+
+def main():
+    get_malc0de()
+    get_alienvault()
+    print d['malc0deips']
+    print d['alienvaultip']
+
+if __name__ == "__main__":
+    main()
+
