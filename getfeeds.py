0e99f850970ee2f018909faf656a6a0e18876d4e
Marius Iversen <pillus@chasenet.org>
2016-11-10 16:25:53 +0100

    finished all the feeds, just need to fix the writefiles

--- getfeeds.py
+++ getfeeds.py
@@ -1,6 +1,5 @@
-import pprint
 import requests
-import json
+import re
 from OTXv2 import OTXv2
 from collections import defaultdict
 from datetime import datetime, timedelta
@@ -9,7 +8,7 @@ from datetime import datetime, timedelta
     File name: getfeeds.py
     Author: Marius Iversen
     Date created: 9/11/2016
-    Date last modified: 9/11/2016
+    Date last modified: 10/11/2016
     Python Version: 2.7.12
 """
 
@@ -23,7 +22,13 @@ def download_file(url):
     return r.content
 
 
-# AlienVault Feed
+# Function you can use to grab only IP's from different types of data
+def ipgrabber(results):
+    ip = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', results)
+    return ip
+
+
+# AlienVault Feed | IP, Domain and URL
 def get_alienvault():
     # Define API Key for AlienVault OTX
     otx = OTXv2("aec68b3658767656b3d93aced4c8f2ea11232c1e2e46acb078692a30a028a174")
@@ -46,22 +51,118 @@ def get_alienvault():
     return d
 
 
-# Malc0de feed
+# Malc0de feed | IP
 def get_malc0de():
     url = 'http://malc0de.com/bl/IP_Blacklist.txt'
     results = download_file(url).splitlines()
     for result in results:
-        result = result.lstrip().rstrip()
-        if not result.startswith('//') and len(result) > 0:
-            d['malc0deips'].append(result)
+        if len(result) > 0:
+            ip = ipgrabber(result.lstrip().rstrip())
+            if ip:
+                d['malc0deip'].append(result)
     return d
 
 
+#Cisco TalosIntel(Old Snort feed) | IP
+def get_talosintel():
+    url = 'http://www.talosintelligence.com/feeds/ip-filter.blf'
+    results = download_file(url).splitlines()
+    for result in results:
+        if len(result) > 0:
+            ip = ipgrabber(result.lstrip().rstrip())
+            if ip:
+                d['talosintelip'].append(result)
+    return d
+
+
+# MalwareDomainList Feed | IP and Domain
+def get_malwaredomainlist():
+    url = 'http://www.malwaredomainlist.com/hostslist/ip.txt'
+    url2 = 'http://www.malwaredomainlist.com/hostslist/hosts.txt'
+    results = download_file(url).splitlines()
+    results2 = download_file(url2).splitlines()
+    for result in results:
+        if len(result) > 0:
+            ip = ipgrabber(result.lstrip().rstrip())
+            if ip:
+                d['malwaredomainlistip'].append(result)
+    for result in results2:
+        result = result.lstrip('127.0.0.1  , ').rstrip()
+        if not result.startswith('#') and not result.startswith('localhost') and len(result) > 0:
+            d['malwaredomainlistdomain'].append(result)
+    return d
+
+
+# Grab all TOR exit nodes
+def get_tornodes():
+    url = 'https://check.torproject.org/exit-addresses'
+    results = download_file(url).splitlines()
+    for result in results:
+        if len(results) > 0:
+            ip = ipgrabber(result.lstrip().rstrip())
+            if ip:
+                d['tornodeip'].append(ip)
+    return d
+
+
+# RansomewareTracker Feed | IP and Domain
+def get_ransomwaretracker():
+    url = 'https://ransomwaretracker.abuse.ch/feeds/csv/'
+    results = download_file(url).splitlines()
+    for result in results:
+        if len(result) > 0:
+            ip = ipgrabber(result.lstrip().rstrip())
+            for singleip in ip:
+                d['ransomwaretrackerip'].append(singleip)
+    for result in results:
+        if re.search(r'\"online\"', result):
+            for url in re.findall(r',\"\w+\.\w+\"', result):
+                url = re.sub(',"', '\"', url)
+                url = re.sub('"', '', url)
+                d['ransomewaretrackerdomain'].append(url)
+    return d
+
+
+# Bambenek security feed | IP and Domain
+def get_bambenek():
+    url = 'https://osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt'
+    url2 = 'http://osint.bambenekconsulting.com/feeds/c2-dommasterlist.txt'
+    results = download_file(url).splitlines()
+    results2 = download_file(url2).splitlines()
+    for result in results:
+        if len(result) > 0:
+            ip = ipgrabber(result.lstrip().rstrip())
+            if ip:
+                d['bambenekip'].append(ip)
+    for result in results2:
+        if not result.startswith('#') and not result.startswith('localhost') and len(result) > 0:
+            url = result.split(",")[0]
+            d['bambenekdomain'].append(url)
+    return d
+
+
+# Main function, that will run all our definitions and create the list
 def main():
-    get_malc0de()
-    get_alienvault()
-    print d['malc0deips']
-    print d['alienvaultip']
+    #get_malc0de()
+    #get_alienvault()
+    #get_talosintel()
+    #get_malwaredomainlist()
+    #get_tornodes()
+    #get_bambenek()
+    get_ransomwaretracker()
+    print "RansomwareTracker IP list is", len(d['ransomwaretrackerip']), " lines long"
+    print "RansomwareTracker Domain list is", len(d['ransomewaretrackerdomain']), " lines long"
+    print "Babenek Domain list is", len(d['bambenekdomain']), " lines long"
+    print "Bambenek IP list is", len(d['bambenekip']), " lines long"
+    print "Tor IP list is",len(d['tornodeip']), " lines long"
+    print "AlienVault IP list is",len(d['alienvaultip']), " lines long"
+    print "AlienVault URL list is", len(d['alienvaulturl']), " lines long"
+    print "AlienVault Domain list is", len(d['alienvaultdomain']), " lines long"
+    print "Malc0deIP list is",len(d['malc0deip']), " lines long"
+    print "TalosintelIP list is",len(d['talosintelip']), " lines long"
+    print "MDLIPs list is",len(d['malwaredomainlistip']), " lines long"
+    print "MDLDomains list is" ,len(d['malwaredomainlistdomain'])," lines long"
+    print [s + "\thello" for s in d['ransomwaretrackerip']]
 
 if __name__ == "__main__":
     main()
